ec63265384f8aaa771d44b9691b459e5d7852eac
jk25084 <aichirenmaoji@163.com>
2019-06-28 16:15:14 +0800

    GenerateExtensionMethods

---
+++ GenerateExtensionMethods/Attributes/DerivedFromAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public class DerivedFromAttribute : Attribute
+{
+    public Type From { get; set; }
+
+    public DerivedFromAttribute(Type from)
+    {
+        From = from;
+    }
+}
---
+++ GenerateExtensionMethods/Sample/MinifyPerson.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VivaLing.Models.Account;
+using VivaLing.Models.BaseData;
+using VivaLing.Pattern.Attributes;
+
+namespace VivaLing.Models.Account
+{
+    // ↑ this namespace must be consistent with the source type
+    [DerivedFrom(typeof(Person))]
+    public partial class MinifyPerson
+    {
+        public string Email { get; set; }
+
+        public Enums.Roles MajorRole { get; set; }
+
+        public MinifyAccount Account { get; set; }
+
+        public List<MinifyPersonExtension> Extensions { get; set; }
+    }
+
+    [DerivedFrom(typeof(Account))]
+    public partial class MinifyAccount
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    [DerivedFrom(typeof(PersonExtension))]
+    public class MinifyPersonExtension
+    {
+        public int Id { get; set; }
+    }
+}
